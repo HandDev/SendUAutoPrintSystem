41599033481117cb1f23ce4bc9813d4cad74d3f0
박재성 <jspark141515@gmail.com>
2016-08-11 22:31:38 +0900

    Created MongoDBManager class for manging connections to MongoDB & get DB

---
+++ PostCardManageSystem/src/main/java/biz/sendu/postcardmanage/mongodb/MongoDBManager.java
@@ -0,0 +1,75 @@
+package biz.sendu.postcardmanage.mongodb;
+
+import com.mongodb.DB;
+import com.mongodb.MongoClient;
+import com.mongodb.ServerAddress;
+
+import java.io.FileInputStream;
+import java.net.UnknownHostException;
+import java.util.Properties;
+
+/**
+ * Created by parkjaesung on 2016. 8. 11..
+ */
+//singleton pattern
+public class MongoDBManager {
+
+    private static MongoDBManager instance = new MongoDBManager();
+
+    private String propURL = System.getProperty("user.dir")+"/sendu/config/db.properties";
+    private Properties dbProps;
+    private MongoClient mongoClient;
+    private DB userDB;
+    private DB ordersDB;
+
+    //private constructor
+    private MongoDBManager(){
+
+        Properties properties = new Properties();
+        /*
+        try {
+            // 프로퍼티 파일 스트림에 담기
+            FileInputStream fis = new FileInputStream(propURL);
+
+            // 프로퍼티 파일 로딩
+            properties.load(new java.io.BufferedInputStream(fis));
+
+            dbProps = properties;
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+
+        //get DB object
+        String ip = dbProps.getProperty("HOST");
+        int port = Integer.parseInt(dbProps.getProperty("PORT"));
+*/
+        try {
+            mongoClient = new MongoClient(new ServerAddress("104.41.183.65", 27017));
+        } catch (UnknownHostException e) {
+            e.printStackTrace();
+        }
+
+        userDB = mongoClient.getDB("users");
+        userDB.authenticate("sendu", "SendUKor1234".toCharArray());
+        ordersDB = mongoClient.getDB("order");
+
+        System.out.println("Setting DB Object finished");
+    }
+
+    public static MongoDBManager getInstance(){
+        return instance;
+    }
+
+    public DB getUsersDB() throws UnknownHostException {
+        return userDB;
+    }
+
+    public DB getOrdersDB() throws UnknownHostException{
+        return ordersDB;
+    }
+
+    public Properties getProps(){
+        return dbProps;
+    }
+
+}

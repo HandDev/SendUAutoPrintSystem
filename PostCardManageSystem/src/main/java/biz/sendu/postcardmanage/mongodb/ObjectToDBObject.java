2fe35a42571df2ed2847bcf49df128d64f6a02a4
박재성 <jspark141515@gmail.com>
2016-08-11 22:32:20 +0900

    Created ObjectToDBObject class for mapping between java class and json

---
+++ PostCardManageSystem/src/main/java/biz/sendu/postcardmanage/mongodb/ObjectToDBObject.java
@@ -0,0 +1,41 @@
+package biz.sendu.postcardmanage.mongodb;
+
+import biz.sendu.postcardmanage.dataclass.Order;
+import biz.sendu.postcardmanage.dataclass.User;
+import com.mongodb.BasicDBObject;
+
+/**
+ * Created by parkjaesung on 2016. 8. 11..
+ */
+
+//Class for changing User & Order object to BasicDocument for Mongo database
+public class ObjectToDBObject {
+
+    private static ObjectToDBObject instance;
+
+    //private constructor
+    private ObjectToDBObject(){
+
+    }
+
+    public static ObjectToDBObject getInstance(){
+        return instance;
+    }
+
+    public BasicDBObject orderToDBObject(Order input){
+        BasicDBObject doc = new BasicDBObject("userUUID",input.getUserUUID()).
+                append("idx", input.getIdx()).
+                append("orderDate", input.getOrderDate()).
+                append("imgDir", input.getImageDIR()).
+                append("status", input.getStatus());
+        return doc;
+    }
+
+    public BasicDBObject userToDBObject(User input){
+        BasicDBObject doc = new BasicDBObject("name", input.getName()).
+                append("uuid",input.getUuid()).
+                append("age", input.getAge());
+
+        return doc;
+    }
+}

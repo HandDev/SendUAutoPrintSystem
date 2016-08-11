0c4fde880839a2abda077f74ccf499e09e79e6c3
박재성 <jspark141515@gmail.com>
2016-08-11 22:32:38 +0900

    TestCase for simple query

---
+++ PostCardManageSystem/src/test/java/biz/sendu/postcardmanage/mongodb/BaseApp.java
@@ -0,0 +1,41 @@
+package biz.sendu.postcardmanage.mongodb;
+
+import com.mongodb.*;
+
+import java.net.UnknownHostException;
+
+/**
+ * Created by parkjaesung on 2016. 8. 11..
+ */
+public class BaseApp {
+
+    public static void main(String[] args){
+        System.out.println(System.getProperty("user.dir"));
+
+        MongoDBManager mongoDBManager = MongoDBManager.getInstance();
+
+        try{
+        mongoDBManager.getOrdersDB();
+        mongoDBManager.getUsersDB();}
+        catch (Exception e){
+            e.printStackTrace();
+        }
+
+        DB db = null;
+        try {
+            db = mongoDBManager.getOrdersDB();
+        } catch (UnknownHostException e) {
+            e.printStackTrace();
+        }
+
+        DBCollection orderTable = db.getCollection("order");
+        BasicDBObject whereQuery = new BasicDBObject();
+        whereQuery.put("uuid", 1234);
+       // DBCursor dbCursor = orderTable.find(whereQuery);
+        DBCursor dbCursor = orderTable.find();
+        while(dbCursor.hasNext()) {
+            System.out.println(dbCursor.next());
+        }
+    }
+
+}
